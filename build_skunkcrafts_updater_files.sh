9502a51e763fc15ff72057e6991906c447ef9b44
telephono <telephono@users.noreply.github.com>
2025-02-15 13:41:20 +0100

    Add support for ignoring files during build

--- build_skunkcrafts_updater_files.sh
+++ build_skunkcrafts_updater_files.sh
@@ -3,9 +3,53 @@
 # Delete old files
 rm -f skunkcrafts_updater_whitelist.txt || true
 rm -f skunkcrafts_updater_sizeslist.txt || true
+rm -f skunkcrafts_updater_blacklist.txt || true
 
-find plugins -type f -print0 | sort -z | while read -d $'\0' file; do
-    echo "${file}"
+# Prepare an empty array for ignored files
+skunkcrafts_updater_ignore_files=()
+
+# Read lines from skunkcrafts_updater_ignore.txt, skipping # comments
+while IFS= read -r line; do
+    # Trim whitespace
+    line="$(echo -e "$line" | tr -d '[:space:]')"
+
+    # Trim leading ./
+    line=${line/#\.\//}
+
+    # Skip empty lines
+    [[ "$line" == "" ]] && continue
+
+    # Skip files that don't exists
+    [[ ! -f "$line" ]] && continue
+
+    # Add line to ignored files
+    skunkcrafts_updater_ignore_files+=("$line")
+done < <(grep -v '^ *#' ./skunkcrafts_updater_ignore.txt | sort)
+
+# Output ignored files, if there are any...
+if [[ "${#skunkcrafts_updater_ignore_files[@]}" != 0 ]]; then
+    printf "Ignoring files:\n"
+    for value in "${skunkcrafts_updater_ignore_files[@]}"; do
+        # Output filename in red
+        printf "\033[0;31m%s\033[0m\n" "$value"
+    done
+    printf "\n"
+fi
+
+printf "Processing files:\n"
+find ./plugins -type f -print0 | sort -z | while read -rd $'\0' file; do
+    # Trim leading ./ if present...
+    file=${file/#\.\//}
+
+    # Ignore .git*
+    [[ $file == .git* ]] && continue
+
+    # Skip files in skunkcrafts_updater_ignore.txt
+    ignored=false
+    for value in "${skunkcrafts_updater_ignore_files[@]}"; do
+        [[ "$file" == "$value" ]] && ignored=true
+    done
+    [[ $ignored == true ]] && continue
 
     # Get CRC32 checksum
     checksum=$(crc32 "${file}")  # hexadecimal
@@ -17,6 +61,9 @@ find plugins -type f -print0 | sort -z | while read -d $'\0' file; do
     elif [[ "${OSTYPE}" == "darwin"* ]]; then
         size=$(stat -f%z "${file}")
     fi
+
+    # Output filename in green
+    printf "\033[0;32m%s\033[0m\n" "${file}"
 
     # Write skunkcrafts_updater_whitelist.txt
     echo "${file}|${checksum}" >> skunkcrafts_updater_whitelist.txt
